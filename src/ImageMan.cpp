d34b4e0b5fa04a154456a20b2b4f236c2fd23594
andy <andy@sarsoo.xyz>
2021-01-30 15:00:32 +0000

    added target cmake stuff, adding functions for manipulating two images

---
+++ src/ImageMan.cpp
@@ -0,0 +1,54 @@
+﻿// ImageMan.cpp : Defines the entry point for the application.
+//
+
+#include "ImageMan/ImageMan.hpp"
+
+using namespace std;
+using namespace cv;
+
+void Resize(Mat &image, int height, int width)
+{
+	resize(image, image, cv::Size(height, width), INTER_AREA);
+}
+
+void SobelFilter(Mat image, int dx = 1, int dy = 0)
+{
+	Sobel(image, image, -1, dx, dy);
+}
+
+void Blur(Mat image)
+{
+	GaussianBlur(image, image, Size(17, 17), 3.0, 3.0);
+}
+
+void Dilate(Mat image, int dilation_size = 5)
+{
+	Mat element = getStructuringElement(MORPH_RECT,
+		Size(2 * dilation_size + 1, 2 * dilation_size + 1),
+		Point(dilation_size, dilation_size));
+	dilate(image, image, element);
+}
+
+int main()
+{
+	Mat clouds = imread("clouds.jpg");
+	Mat holo = imread("holoshot.png");
+	cout << "image size: (" << holo.size() << ")" << endl;
+	try {
+		//SobelFilter(holo);
+		Dilate(holo);
+		Blur(holo);
+
+		Resize(clouds, 800, 400);
+		SobelFilter(clouds, 0, 1);
+		Dilate(clouds);
+	
+		imshow("holo", holo);
+		imshow("clouds", clouds);
+		waitKey();
+	}
+	catch (cv::Exception e) {
+		cout << "error: " << e.what() << endl;
+	}
+	return 0;
+}
